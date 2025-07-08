07830aaf0a97bc7fea049fc44155cea69f7c2736
BooleanBile <oarabile.marwane1@gmail.com>
2025-07-08 17:54:19 +0200

    part 1 of part 2 done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace IceTask2_BooleanBile
+﻿using System.Collections;
+
+namespace IceTask2_BooleanBile
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //arraylist of five student objects
+            // not allowed List<T>
+            ArrayList students = new ArrayList();
+
+            students.Add(new Student( "NoThando", 98, 87));
+            students.Add(new Student("Alex", 68, 47));
+            students.Add(new Student("Joyce", 74, 96));
+            students.Add(new Student("Oarabile", 37, 77));
+            students.Add(new Student("Mfundo", 63, 55));
+
+            //display details of students
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students.ToString());
+                
+            }
         }
     }
 }
